bef1be13e40b145387aa573b3ce7edec63599c9e
Kathryn Cheng <yiwenchengutd@gmail.com>
2021-02-13 22:20:16 -0800

    Add requirements and config loader.

--- domain_monitor/__init__.py
+++ domain_monitor/__init__.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 # app should read config from application.cfg
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/app.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-app.config.from_object('config')
+app.config.from_object('domain_monitor.config')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
